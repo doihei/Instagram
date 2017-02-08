3f24e2f305355f160fdb2b3590fef61ef40d0cf0
daihei.doi <dd11197208@gmail.com>
2017-02-08 18:31:33 +0900

    AdobeCreativeSDK使うためのプロジェクト設定

---
+++ Instagram/Instagram-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Instagram-Bridging-Header.h
+//  
+//
+//  Created by 土井大平 on 2017/02/08.
+//
+//
+
+#ifndef Instagram_Bridging_Header_h
+#define Instagram_Bridging_Header_h
+
+#import <AdobeCreativeSDKCore/AdobeCreativeSDKCore.h>
+#import <AdobeCreativeSDKImage/AdobeCreativeSDKImage.h>
+
+#endif /* Instagram_Bridging_Header_h */
